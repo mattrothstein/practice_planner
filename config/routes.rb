d3e40211a10a7c207411d10de4555bf6ab4cd75f
Matt <matthewrothstein@hotmail.com>
2016-09-10 20:12:20 -0400

    add route for dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root to: 'sessions#new'
   resources :sessions
+  get 'dashboard' => 'dashboard#dashboard',  as: 'dashboard' 
+  # resources :dashboard
   resources :users do
     resources :teams
   end
